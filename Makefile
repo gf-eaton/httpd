329ab88144f3ed3404947e9aa1683449980ae172
gf <guyfrancoeur@eaton.com>
2023-10-28 19:42:00 -0400

    Add project files.

---
+++ Makefile
@@ -0,0 +1,26 @@
+httpd: httpd.cpp
+	apt install -y libpqxx-dev
+	g++ $^ -O3 -I/usr/include/ -std=c++20 -lpqxx -o $@
+
+test: httpd
+	/usr/bin/time -f"Executed:%es\nRAM:%MKb\nSystem:%Ss" ./mca --runfor 00:01 --id 1 --assetid 1 --config ./config-mca.json
+	/usr/bin/time -f"Executed:%es\nRAM:%MKb\nSystem:%Ss" ./mca --runfor 0000000
+
+httpd-arm64: httpd.cpp
+	apt install -y g++-12-aarch64-linux-gnu gcc-12-aarch64-linux-gnu
+	apt install -y libpqxx-dev:arm64
+	aarch64-linux-gnu-g++-12 $^ -O3 -I/usr/include/modbus/ -I/usr/include/ -L/usr/lib/aarch64-linux-gnu/ -std=c++20 -lpqxx -o $@
+
+mca-pack: httpd-arm64
+	rm -f httpd-arm64.zip
+	cp /usr/lib/aarch64-linux-gnu/libpqxx-6.4.so .
+	zip httpd-arm64.zip libpqxx-6.4.so httpd-arm64
+	rm -f libpqxx-6.4.so
+
+requirements.txt:
+	git clone https://github.com/ithewei/libhv.git
+	cd libhv
+	./configure
+	make
+	make install
+	touch ../requirements.txt
---
+++ httpd.cpp
@@ -0,0 +1,81 @@
+﻿#include "hv/HttpServer.h"
+using namespace hv;
+#include <chrono>   //chrono C++20
+#include <pqxx/pqxx> //postgres libpqxx C++
+
+int main() {
+  HttpService router;
+  router.GET("/ping", [](HttpRequest* req, HttpResponse* resp) {
+    return resp->String("pong");
+    });
+
+  router.GET("/data", [](HttpRequest* req, HttpResponse* resp) {
+    static char data[] = "0123456789";
+    return resp->Data(data, 10);
+    });
+
+  router.GET("/paths", [&router](HttpRequest* req, HttpResponse* resp) {
+    return resp->Json(router.Paths());
+    });
+
+  router.GET("/get", [](HttpRequest* req, HttpResponse* resp) {
+    resp->json["origin"] = req->client_addr.ip;
+    resp->json["url"] = req->url;
+    resp->json["args"] = req->query_params;
+    resp->json["headers"] = req->headers;
+    return 200;
+    });
+
+  router.POST("/echo", [](const HttpContextPtr& ctx) {
+    return ctx->send(ctx->body(), ctx->type());
+    });
+
+  router.GET("/api", [](HttpRequest* req, HttpResponse* resp) {
+    auto start = std::chrono::steady_clock::now();
+    try
+    {
+      pqxx::connection conn("user=pxmcea host=166.99.230.91 password=Security*8 dbname=telemetry");
+      pqxx::transaction tx{ conn };
+      for (auto const& [k, c] : tx.stream<std::string_view, long>("SELECT 'count', count(*) c FROM telemetry"))
+      {
+        //std::cout << att << ": " << count << '\n';
+        resp->json["count"] = c;
+      }
+      conn.close();
+    }
+    catch (std::exception const& e)
+    {
+      std::cerr << "ERROR: " << e.what() << '\n';
+      return 400;
+    }
+    resp->json["origin"] = req->client_addr.ip;
+    resp->json["url"] = req->url;
+    resp->json["args"] = req->query_params;
+    resp->json["headers"] = req->headers;
+    auto t0 = std::chrono::steady_clock::now() - start;
+    resp->json["timer_ms"] = std::chrono::duration_cast<std::chrono::milliseconds>(t0).count();
+    return 200;
+    });
+
+  HttpServer server = HttpServer();
+  server.registerHttpService(&router);
+  server.setPort(8080);
+  server.setThreadNum(4);
+  server.run();
+  return 0;
+}
+
+//sample 1
+/*
+pqxx::result r{ txn.exec("SELECT name, salary FROM Employee") };
+for (auto row : r)
+std::cout << row["name"].c_str() << " makes " // Address column by name.  Use c_str() to get C-style string.
+<< row[1].as<int>() << "$." << std::endl; // Address column by zero-based index.  Use as<int>() to parse as int.
+*/
+//sample 2
+/*
+  long total = 0;
+  for (auto [salary] : txn.query("SELECT salary FROM Employee"))
+    total += salary;
+  std::cout << "Total salary: " << total << '\n';
+*/
